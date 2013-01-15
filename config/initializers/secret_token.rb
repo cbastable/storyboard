8d555db876ae21ccd588373073307c0af87e6fd4
cbastable <therightstuff17@gmail.com>
2013-01-15 18:50:14 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Storyboard::Application.config.secret_token = '9f7f12b6b40a0e0662c744830510d4f185c1c7095a2dda132bdbf4b1cbcb65d4d16889f25cb4066ccd46a7d5ac9c2cdf6b5f656497fb025bca96e10a75ae09f9'
