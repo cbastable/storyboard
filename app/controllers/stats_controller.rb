2f578a73be39bdb1154b7898312f8028b48fac6d
cbastable <therightstuff17@gmail.com>
2013-04-26 21:21:32 -0400

    Add disabled state for modal buttons when story is free or user not logged in. Remove profile cog in header. Janky if statements everywhere, not DRY

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -14,11 +14,17 @@ class StatsController < ApplicationController
       @stats = @story.stats.find_by_viewer_id(current_user.id)
       @board = current_user.boards.create!(name: "Read", story_id: @story.id)
       sign_in User.find_by_id(current_user.id)
-    end
-    
-        if params[:commit] == 'Pay with Community Points' && current_user.community_points >= @story.price
+    elsif params[:commit] == 'Pay with Community Points' && current_user.community_points >= @story.price
       @balance = current_user.community_points
       current_user.update_community_points!(current_user, @balance - @story.price)
+      @stat = Stat.new(params[:stat])
+      @stat.story.add_to_stats!(@stat)
+      @story = @stat.story
+      @user = @story.user
+      @stats = @story.stats.find_by_viewer_id(current_user.id)
+      @board = current_user.boards.create!(name: "Read", story_id: @story.id)
+      sign_in User.find_by_id(current_user.id)
+    elsif params[:commit] == 'View story for free' && @story.price == 0
       @stat = Stat.new(params[:stat])
       @stat.story.add_to_stats!(@stat)
       @story = @stat.story
