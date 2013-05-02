0b2ab05d9ad698927ebbf25fefd64349437c1da4
cbastable <therightstuff17@gmail.com>
2013-05-02 10:02:28 -0400

    Re-add production minues stripe keys

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -4,14 +4,3 @@ Rails.configuration.stripe = {
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
-#####
-#if Rails.env == "production"
-  # set credentials from ENV hash  
-#  stripe = {"app_id" => ENV['PUBLISHABLE_KEY'], "secret" => ENV['SECRET_KEY']}
-#else
-  # get credentials from YML file
-#  stripe = YAML.load(File.open(Rails.root.join("config/stripe.yml")))
-#end
- 
-#Stripe.api_key = stripe["secret"]
-#STRIPE_PUBLIC_KEY = stripe["app_id"]
